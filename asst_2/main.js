c8bd17bfd980ce8cd74f0039d899084d5356d1d1
echoy1 <60661592+echoy1@users.noreply.github.com>
2020-04-02 20:17:27 -0700

    Update main.js

--- asst_2/main.js
+++ asst_2/main.js
@@ -16,7 +16,7 @@ for (i = 1; i < 6; i++) {
 
 /* Wiring up the Darken/Lighten button */
 btn.onclick = function() {
-    if (btn.getAttribute('class') == "dark") {
+    if (btn.getAttribute('class') === "dark") {
         btn.setAttribute('class', "light");
         btn.textContent = "Lighten";
         overlay.style.backgroundColor =  "rgba(0,0,0,0.5)";
